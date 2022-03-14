1ead72d5bd1ed2e10a9be5ce8ca1b55d7d36e802
Varvara Kiseleva <50034579+varvarakisel@users.noreply.github.com>
2022-03-14 23:28:18 +0300

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -1,5 +1,5 @@
 import os
 
 API_KEY = os.environ.get("API_KEY","")
-VK_API_KEY = os.environ.get("VK_API_KEYSSSS","b67963b1b67963b1b67963b1d1b602c780bb679b67963b1d456ea3eecb16115d1b79c32")
+VK_API_KEY = os.environ.get("VK_API_KEYS","")
 GROUP_ID = os.environ.get("GROUP_ID", "57721072")
